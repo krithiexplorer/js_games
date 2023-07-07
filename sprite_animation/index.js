710780e62e465bc57d9eb00e3d24b6935ba23589
krithiexplorer <kiruthiga.kameswaran@gmail.com>
2023-07-07 21:35:25 +0530

    Dynamic animation

--- sprite_animation/index.js
+++ sprite_animation/index.js
@@ -4,22 +4,40 @@ const CANVAS_WIDTH = canvas.width = 400;
 const CANVAS_HEIGHT = canvas.height = 400;
 
 const spriteImage = new Image();
-spriteImage.src = 'girl.png';
-let spriteWidth = 120;
-let spriteHeight = 133;
+spriteImage.src = 'shadow_dog.png';
+let spriteWidth = 575;
+let spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
 const staggerFrame = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name:'idle',
+        frames : 7
+    },
+    {
+        name:'jump',
+        frames: 7
+    },
+    {
+        name:'down',
+        frames:7
+    }
+]
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc : []
+    }   
+    
+})
 function animate()
 {
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.drawImage(spriteImage,frameX * spriteWidth,frameY * spriteHeight,spriteWidth,spriteHeight,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    if(gameFrame % staggerFrame == 0)
-    {
-        if(frameX < 8)frameX++;
-        else frameX=0;
-    }
+    let position = Math.floor(gameFrame/staggerFrame)%6;
+    frameX = spriteWidth * position;
+    ctx.drawImage(spriteImage,frameX,frameY * spriteHeight,spriteWidth,spriteHeight,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     gameFrame++;
     requestAnimationFrame(animate);
 }
