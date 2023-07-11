4b7f3b4c81ac9069e83d5e0550bf44b077ae7321
krithiexplorer <kiruthiga.kameswaran@gmail.com>
2023-07-11 21:06:28 +0530

    update and draw methods

--- parallax_background/index.js
+++ parallax_background/index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
-let gameSpeed = 15;
+let gameSpeed = 5;
 let x = 0;
 
 const backgroundLayer1 = new Image();
@@ -27,7 +27,29 @@ class Layer
         this.image = image;
         this.speedModifier = speedModifier;
     }
+    update()
+    {
+        this.speed = gameSpeed * this.speedModifier;
+        if(this.x <= -this.width)
+            this.x = this.width + this.x2 - this.speed;
+        if(this.x2 <= -this.width)
+            this.x2 = this.width + this.x - this.speed;
+
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);    
+    }
+    draw()
+    {
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+        ctx.drawImage(this.image,this.x2,this.y,this.width,this.height);
+    }
 }
+
+const layer1 = new Layer(backgroundLayer1,1);
+const layer2 = new Layer(backgroundLayer2,1);
+const layer3 = new Layer(backgroundLayer3,1);
+const layer4 = new Layer(backgroundLayer4,1);
+const layer5 = new Layer(backgroundLayer5,1);
 
 function animate()
 {
