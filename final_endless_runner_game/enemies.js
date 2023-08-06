b502cbcb40ba513ca6d9fdd754e7c6a3fea5f176
krithiexplorer <kiruthiga.kameswaran@gmail.com>
2023-08-06 12:39:12 +0530

    enemies done

--- final_endless_runner_game/enemies.js
+++ final_endless_runner_game/enemies.js
@@ -7,10 +7,11 @@ class Enemy
         this.fps = 20;
         this.frameInterval = 1000/this.fps;
         this.frameTimer = 0;
+        this.markedForDeletion = false;
     }
     update(deltaTime)
    {
-        this.x += this.speedX;
+        this.x -= this.speedX + this.game.speed;
         this.y += this.speedY;
         if(this.frameTimer > this.frameInterval)
         {
@@ -21,6 +22,8 @@ class Enemy
         else{
             this.frameTimer += deltaTime;
         }
+
+        if(this.x + this.width < 0) this.markedForDeletion = true;
    } 
    draw(context)
    {
@@ -37,26 +40,70 @@ export class FlyingEnemy extends Enemy
         this.game = game;
         this.width = 60;
         this.height = 44;
-        this.x = this.game.width;
+        this.x = this.game.width + Math.random() * this.game.width * 0.5;
         this.y = Math.random() * this.game.height * 0.5;
-        this.speedX = 2;
+        this.speedX = Math.random() + 1;
         this.speedY = 0;
         this.maxFrame = 5; 
         this.image = new Image();
         this.image.src = 'assets/enemy_fly.png';
+        this.angle = 0;
+        this.velocity = Math.random() * 0.1;
     }
     update(deltaTime)
     {
         super.update(deltaTime);
+        this.angle += this.velocity;
+        this.y += Math.sin(this.angle);
     }
 }
 
 export class GroundEnemy extends Enemy
 {
-
+    constructor(game)
+    {
+        super();
+        this.game = game;
+        this.width = 60;
+        this.height = 87;
+        this.x = this.game.width;
+        this.y = this.game.height - this.height - this.game.groundMargin;
+        this.image = new Image();
+        this.image.src = 'assets/enemy_plant.png';
+        this.speedX = 0;
+        this.speedY = 0;
+        this.maxFrame = 1;
+    }
 }
 
 export class ClimbingEnemy extends Enemy
 {
-
+    constructor(game)
+    {
+        super();
+        this.game = game;
+        this.width = 120;
+        this.height = 144;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.image = new Image();
+        this.image.src = 'assets/enemy_spider_big.png';
+        this.speedX =0;
+        this.speedY = Math.random() > 0.5 ? 1 : -1;
+        this.maxFrame = 5;
+    }
+    update(deltaTime)
+    {
+        super.update(deltaTime);
+        if(this.y > this.game.height - this.height - this.game.groundMargin) this.speedY *= -1;
+        if(this.y < -this.height) this.markedForDeletion = true;
+    }
+    draw(context)
+    {
+        super.draw(context);
+        context.beginPath();
+        context.moveTo(this.x + this.width/2,0);
+        context.lineTo(this.x + this.width/2, this.y + 50);
+        context.stroke();
+    }
 }
--- final_endless_runner_game/main.js
+++ final_endless_runner_game/main.js
@@ -30,7 +30,7 @@ window.addEventListener('load',function()
         {
             this.background.update();
             this.player.update(this.input.keys, deltaTime);
-            if(enemyTimer > this.enemyInterval)
+            if(this.enemyTimer > this.enemyInterval)
             {
                 this.addEnemy();
                 this.enemyTimer = 0;
@@ -42,6 +42,10 @@ window.addEventListener('load',function()
 
             this.enemies.forEach(enemy =>{
                 enemy.update(deltaTime);
+                if(enemy.markedForDeletion) 
+                {
+                    this.enemies.splice(this.enemies.indexOf(enemy),1);
+                }
             })
         }
         draw(context)
@@ -54,7 +58,16 @@ window.addEventListener('load',function()
         }
         addEnemy()
         {
+            if(this.speed > 0 && Math.random() > 0.5) 
+            {
+                this.enemies.push(new GroundEnemy(this));
+            }
+            else if(this.speed > 0)
+            {
+                this.enemies.push(new ClimbingEnemy(this));
+            }
             this.enemies.push(new FlyingEnemy(this));
+            console.log(this.enemies);
         }
     }
     const game = new Game(canvas.width, canvas.height);
